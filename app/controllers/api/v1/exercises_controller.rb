0a2e33cc2009ea4bde57581936382caad3caf2df
Michael Salerno <msalerno1993@gmail.com>
2022-03-06 02:04:28 -0500

    set up controllers

--- app/controllers/api/v1/exercises_controller.rb
+++ app/controllers/api/v1/exercises_controller.rb
@@ -1,2 +1,43 @@
 class Api::V1::ExercisesController < ApplicationController
+   before_action :set_workout
+
+
+    def index
+        @exercises = @workout.exercises
+        render json: @exercises
+    end
+
+    def create
+        @exercise = @workout.exercises.new(exercise_params)
+        if @exercise.save
+            render json: @exercise
+        else
+            render json: {error: 'Error Creating Exercise'}
+        end
+    end
+
+    def show
+        @exercise = @workout.exercises.find_by(id: params[:id])
+        render json: @exercise
+    end
+
+    def destroy
+        @exercise = @workout.exercises.find_by(id: params[:id])
+        @exercise.destroy
+    end
+
+
+    private
+
+    def set_workout
+        @workout = Workout.find(params[:workout_id])
+    end
+
+
+    def exercise_params
+        params.require(:exercise).permit(:workout_id, :name, :muscle_group, :weight, :reps, :sets, :exercise_note)
+    end
+
+
 end
+
--- app/controllers/api/v1/workouts_controller.rb
+++ app/controllers/api/v1/workouts_controller.rb
@@ -1,2 +1,34 @@
 class Api::V1::WorkoutsController < ApplicationController
+
+    def index
+        @workouts = Workouts.all
+        render json: @workouts
+    end
+
+    def create
+        @workout = Workout.new(workout_params)
+        if @workout.save
+            render json: @workout
+        else
+            render json: {error: 'Error Creating Workout'}
+        end
+    end
+
+    def show
+        @workout = Workout.find(params[:id])
+        render json: @workout
+    end
+
+    def destroy
+        @workout = Workout.find(params[:id])
+        @workout.destory
+    end
+
+
+    private
+
+    def workout_params
+        params.require(:workout).permit(:title, :date, :workout_note)
+    end
+
 end
